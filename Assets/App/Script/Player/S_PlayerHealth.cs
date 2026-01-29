a79dbe7453cf7d738a7db019f6e87d2b9a68924f
Verdraks <martinezorlando955@gmail.com>
2026-01-29 15:54:14 +0100

    Feat: Feedback hit

--- Assets/App/Script/Player/S_PlayerHealth.cs
+++ Assets/App/Script/Player/S_PlayerHealth.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using UnityEngine.Events;
 
-public class S_PlayerTakeDamage : MonoBehaviour
+public class S_PlayerHealth : MonoBehaviour
 {
     //[Header("Settings")]
     //[Header("References")]
@@ -8,8 +9,10 @@ public class S_PlayerTakeDamage : MonoBehaviour
     [SerializeField] private RSE_BasicEvent onTakeDamage;
     [Header("Output")]
     [SerializeField] private RSO_Int rso_playerHealth;
+    [SerializeField] private UnityEvent onHealthChanged;
 
     private int health;
+    
     private void OnEnable()
     {
         onTakeDamage.Action += TakeDamage;
@@ -22,5 +25,6 @@ public class S_PlayerTakeDamage : MonoBehaviour
     {
         health++;
         rso_playerHealth.Set(health);
+        onHealthChanged.Invoke();
     }
 }
